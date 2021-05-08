06d617b6cd1f43e16db5c049b6a4b91635b33dd0
sperospera1225@gmail.com <sperospera1225@gmail.com>
2021-05-08 19:30:28 +0900

    week1

--- w1_1.py
+++ w1_1.py
@@ -8,9 +8,9 @@ if len(duplicate_set) == len(duplicate_list):
     print("중복이 없습니다.")
 else:
     print("중복이 있습니다.")
-# #2) 순열 확인 : 문자열 두 개가 주어졌을 때 이 둘이 서로 순열 관계에 있는지 확인하는 메서드를 작성하라.
-# word1, word2 = map(str, input().split())
-# # 3) 회문 수열 : 주어진 문자열이 회문(palindrome)의 순열인지 아닌지 확인하는 함수를 작성하라. 회문이란 앞으로 읽으나 뒤로 읽으나 같은 단어 혹은 구절을 의미하며, 순열이란 문자열을 재배치하는 것을 뜻한다.
+# # #2) 순열 확인 : 문자열 두 개가 주어졌을 때 이 둘이 서로 순열 관계에 있는지 확인하는 메서드를 작성하라.
+# # word1, word2 = map(str, input().split())
+# # # 3) 회문 수열 : 주어진 문자열이 회문(palindrome)의 순열인지 아닌지 확인하는 함수를 작성하라. 회문이란 앞으로 읽으나 뒤로 읽으나 같은 단어 혹은 구절을 의미하며, 순열이란 문자열을 재배치하는 것을 뜻한다.
 word = input()
 is_palindrome = True
 for i in range(len(word)):
@@ -24,7 +24,7 @@ print(is_palindrome)
 
 # 5) 문자열 압축 : 반복되는 문자의 개수를 세는 방식의 기본적인 문자열 압축 메서드를 작성하라. 예를 들어, 문자열 aabccccaaa 압축하면 a2b1c5a3이 된다. 만약 압축한 문자열의 길이가 기존 문자열의 길이보다 길다면 기존 문자열을 반환해야 한다. 문자열은 a-z 으로만 이루어져있다.
 string_compress = input()
-temp = string_compress[0]
+temp = string_compress[0:1]
 count = 0
 result = ''
 
